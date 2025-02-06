ef78efc56f61877fbbd429983b302551bf5876c1
dvsys <dvsys@localhost.localdomain>
2025-02-06 13:32:09 +0100

    update

--- main.py
+++ main.py
@@ -6,16 +6,39 @@
 import os
 import json
 import threading
+import shutil
+from time import sleep
+
+from termcolor import colored
 from concurrent.futures import ThreadPoolExecutor
+
+tools = [
+    'shuffledns','subfinder','assetfinder','dnsx','puredns',
+    'scilla'
+]
 
 CONFIGURATION_FILE = 'config.json' 
 with open (CONFIGURATION_FILE, 'r') as file :
     data = json.load (file)
-
-OUTPUT_PATH = data['output']['filename']
+SAVE_FOLDER = data['output']['filename']
 #OUTPUT_PATH = data.get ('output', {}).get('filename', 'Not found')
+def check_folder (folder_name):
+    if not os.path.exists (folder_name):
+        os.mkdir (folder_name)
+        print (f'Folder was created {folder_name}')
+
+def tools_check (tool):
+    check_tool_result = shutil.which (tool)
+    if check_tool_result : print (colored(f'{tool} i found !','green'))
+    else : print (colored (f'{tool} is not found', 'red'))
 
 
-print (OUTPUT_PATH)
+
+for tool in tools :
+    tools_check (tool)
+
+check_folder (SAVE_FOLDER)
+
+
 
 
